441ed16f5f17c832d1bbfc89a9cf0c2374c6107c
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-10-20 06:39:05 +0900

    セルを追加

--- testTableview/TableViewController.swift
+++ testTableview/TableViewController.swift
@@ -13,6 +13,23 @@ class TableViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 3
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "testCell", for: indexPath)
+        
+        cell.textLabel?.text = "test"
+        
+        return cell
+        
+    }
+
 
 
 }
