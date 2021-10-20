95b2e7fd396150948b1d6176e57030f52b0c895b
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-10-21 06:55:00 +0900

    Navigation Controllerを追加

--- testTableview/TableViewController.swift
+++ testTableview/TableViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return 3
@@ -29,8 +27,8 @@ class TableViewController: UITableViewController {
         return cell
         
     }
-
-
-
+    
+    
+    
 }
 
