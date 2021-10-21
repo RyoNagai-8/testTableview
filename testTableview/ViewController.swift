79cc44c94019c5f982e304700eed025b1dc457f4
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-10-22 06:22:25 +0900

    クラス名を変更

--- testTableview/ViewController.swift
+++ testTableview/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
